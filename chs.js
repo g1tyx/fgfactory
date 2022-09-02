49380b3a47c9f73a698e7c4222ad34bd72fcedc4
zhaolinxu <zhaolinxu@sina.cn>
2022-09-02 16:48:41 +0800

    hh

--- chs.js
+++ chs.js
@@ -564,6 +564,7 @@ var cnRegReplace = new Map([
     [/^You are gaining (.+) elves per second$/, '你每秒获得 $1 精灵'],
     [/^You have (.+) points$/, '你有 $1 点数'],
     [/^Next at (.+) points$/, '下一个在 $1 点数'],
+	[/^Top ([\d\.]+) World ([\d\.]+)$/, 'Top $1 世界 $2'],
 	[/^([\d\.]+)\/sec$/, '$1\/秒'],
 	[/^([\d\.,]+)\/sec$/, '$1\/秒'],
 	[/^([\d\.,]+) OOMs\/sec$/, '$1 OOMs\/秒'],
